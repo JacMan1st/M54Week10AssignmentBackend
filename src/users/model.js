1074c24491156759da7591e3b824438fe4deaa98
Jack Mears <jack.mears@ymail.com>
2024-02-28 19:24:58 +0000

    enhancement: change to connection between FE and BE

--- src/users/model.js
+++ src/users/model.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../db/conections");
+const sequelize = require("../db/connections");
 
 const User = sequelize.define("User", {
   username: {
