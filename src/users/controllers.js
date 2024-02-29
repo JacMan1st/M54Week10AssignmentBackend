d6e74311ef0dc4cac088b54c6064c1edc852b3c8
Jack Mears <jack.mears@ymail.com>
2024-02-29 17:59:00 +0000

    enhancement: update contollers to older version

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -15,17 +15,7 @@ const signupUser = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    const isPasswordValid = await user.comparePassword(req.body.password);
-
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-
-    res.status(200).json({ message: "Login successful", user: user });
+    res.status(200).json({ message: "login successful", user: req.user });
   } catch (error) {
     res.status(500).json({ message: error.message, error: error });
   }
